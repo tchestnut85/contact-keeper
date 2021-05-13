2045608816938cbefc459f1ff6101f283b2658fc
Tom Chestnut <tchestnut85@gmail.com>
2021-05-12 21:11:33 -0400

    Added the post route to add/create a contact.

--- routes/contacts.js
+++ routes/contacts.js
@@ -17,7 +17,30 @@ router.get('/', auth, async (req, res) => {
 });
 
 // POST a new contact - /api/contacts
-router.post('/', (req, res) => res.send('Add a new contact'));
+router.post('/', [auth, [check('name', 'Name is required.').not().isEmpty()]], async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	const { name, email, phone, type } = req.body;
+
+	try {
+		const newContact = new Contact({
+			name,
+			email,
+			phone,
+			type,
+			user: req.user.id,
+		});
+
+		const contact = await newContact.save();
+
+		res.json(contact);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error!');
+	}
+});
 
 // PUT update a contact - /api/contacts
 router.put('/:id', (req, res) => res.send('Update a contact'));
