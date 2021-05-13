fe2152d036d58bf83101b5b12cc88ff1cb36a4b2
Tom Chestnut <tchestnut85@gmail.com>
2021-05-12 20:28:43 -0400

    Added login route with bcrypt to verify a password and jwt for user authentication.

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+
+const mySecret = process.env.JWT_SECRET;
 
 // GET users at /api/auth
 router.get('/', (req, res) => {
@@ -7,6 +13,54 @@ router.get('/', (req, res) => {
 });
 
 // POST to login a user at /api/auth
-router.post('/', (req, res) => res.send('Login a user'));
+router.post(
+	'/',
+	[
+		check('email', 'Please include a valid email.').isEmail(),
+		check('password', 'Password is required.').exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res.status(400).json({ message: 'Invalid Credentials!' });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+			if (!isMatch) {
+				return res.status(400).json({ message: 'Invalid Credentials!' });
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				mySecret,
+				{
+					expiresIn: '1h',
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error!');
+		}
+	}
+);
 
 module.exports = router;
