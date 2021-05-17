9c54cf1c08f65cba845bfd6a946465ebef3965df
Tom Chestnut <tchestnut85@gmail.com>
2021-05-17 19:48:15 -0400

    Setup React front-end home and about pages, navbar, and implemented React Router for page routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Navbar from './components/layout/Navbar';
+import React from 'react';
+
+const App = () => {
+	return (
+		<Router>
+			<>
+				<Navbar />
+				<div className='container'>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/about' component={About} />
+					</Switch>
+				</div>
+			</>
+		</Router>
+	);
+};
 
 export default App;
---
+++ client/src/components/layout/Navbar/index.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Navbar = ({ title, icon }) => {
+	return (
+		<div className='navbar bg-primary'>
+			<h1>
+				<i className={icon} />
+				{title}
+			</h1>
+			<ul>
+				<li>
+					<Link to='/'> Home</Link>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+	title: 'Contact Keeper',
+	icon: 'fas fa-id-card-alt',
+};
+
+export default Navbar;
