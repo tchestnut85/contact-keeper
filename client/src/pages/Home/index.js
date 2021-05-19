cd06894d85800a15ec60aebce830cb23aa3affe0
Tom Chestnut <tchestnut85@gmail.com>
2021-05-19 19:17:46 -0400

    Setup contact-form component and imported into the homepage.

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,13 @@
+import ContactForm from '../../components/contacts/ContactForm';
 import Contacts from '../../components/contacts/Contacts';
 import React from 'react';
 
 const Home = () => {
 	return (
 		<main className='grid-2'>
-			<div>{/* contact form */}</div>
+			<div>
+				<ContactForm />
+			</div>
 			<div>
 				<Contacts />
 			</div>
