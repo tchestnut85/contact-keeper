20fa6f198d6df58165cc8321a1ef7ad597361233
Tom Chestnut <tchestnut85@gmail.com>
2021-05-20 20:00:24 -0400

    Setup state for user authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import About from './pages/About';
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 import Home from './pages/Home';
 import Navbar from './components/layout/Navbar';
@@ -10,19 +11,21 @@ import React from 'react';
 
 const App = () => {
 	return (
-		<ContactState>
-			<Router>
-				<>
-					<Navbar />
-					<div className='container'>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<Route exact path='/about' component={About} />
-						</Switch>
-					</div>
-				</>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<Router>
+					<>
+						<Navbar />
+						<div className='container'>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/about' component={About} />
+							</Switch>
+						</div>
+					</>
+				</Router>
+			</ContactState>
+		</AuthState>
 	);
 };
 
---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,38 @@
+import { REGISTER_FAIL, REGISTER_SUCCESS } from '../actions';
+import React, { useReducer } from 'react';
+
+import AuthContext from './authContext';
+import authReducer from './authReducer';
+
+const AuthState = props => {
+	const initialState = {
+		token: localStorage.getItem('contactsAppToken'),
+		user: null,
+		isAuthenticated: null,
+		loading: true,
+		error: null,
+	};
+	const [state, dispatch] = useReducer(authReducer, initialState);
+
+	// Load user
+
+	// Register use
+
+	// Login user
+
+	// Clear errors
+
+	return (
+		<AuthContext.Provider
+			value={{
+				token: state.token,
+				user: state.user,
+				isAuthenticated: state.isAuthenticated,
+				loading: state.loading,
+				error: state.error,
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
+};
